e0d5d2f7cd3685918e2cf4ae8002ae4556c828d1
Masralai <devdeeppaul2004@gmail.com>
2025-01-03 02:14:08 +0530

    oops l1

--- L42/class.cpp
+++ L42/class.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 //#include"hero.cpp"
 
+//https://www.naukri.com/code360/guided-paths/basics-of-c/content/118817/offering/1382190
+
 using namespace std;
 
 
@@ -15,10 +17,13 @@ class hero{
 
     //
     public:
+    static int timeToComplete;
+    char *name;
     char level;
 
     hero(){
         cout<<"simple constructor called"<<endl;
+        name = new char[100];
     }
 
 
@@ -36,6 +41,10 @@ class hero{
     //copy constructor
     hero(hero& temp){
 
+        char *ch= new char[strlen(temp.name)+1];
+        strcpy(ch,temp.name);
+        this -> name = ch;
+
         cout<<"copy constructor called "<<endl;
         this -> level= temp.level;
         this -> health= temp.health;  
@@ -43,7 +52,8 @@ class hero{
 
 
     void print(){
-        cout<<"health is "<<this->health<<endl;
+        cout<<"name is "<<this->name<<",";
+        cout<<"health is "<<this->health<<",";
         cout<<"level is "<<this->level<<endl;
     }
 
@@ -59,11 +69,84 @@ class hero{
     void setLevel(char ch){
         level = ch;
     }
+    void setName(char name[]){
+        strcpy(this->name,name);
+    }
 
+
+    static int random (){
+        //cout<<this->health<<endl;
+        //cout<<health<<endl;
+        //return health;
+        return timeToComplete;
+    }
+
+
+    ~hero(){ //destructor
+        cout<<"destructor bhai called"<<endl;
+    }
 
 };
+
+int hero :: timeToComplete = 5;
  
 int main() {
+
+    //cout<<hero::timeToComplete<<endl;
+
+    cout<<hero::random()<<endl;
+
+
+
+
+
+    //  hero a;
+    //  cout<<a.timeToComplete<<endl; 
+    //  //not recommended as it does not belong to objec
+    //  hero b;
+    //  b.timeToComplete=10;//updated due to b
+    //  cout<<a.timeToComplete<<endl; 
+    //  cout<<b.timeToComplete<<endl; 
+
+ 
+
+    // //static
+    // hero a;
+
+
+    // //dynamic
+    // hero *b = new hero;
+    // delete b; //manually calling destructor for dynamic allocation
+
+
+
+
+
+    // hero hero1;
+
+    // hero1.setHealth(12);
+    // hero1.setLevel('D');
+    // char name[7]="babbar";
+    // hero1.setName(name);
+    // //hero1.print();
+
+    // //use default copy constructor
+    // hero hero2(hero1);
+    // //hero hero2 = hero1;
+    // //hero2.print();
+
+    // hero1.name[0]= 'G';
+    
+    // hero1.print();
+    // hero2.print();
+
+    // hero1=hero2; //copy assignment operator
+
+    // hero1.print();
+    // hero2.print();
+
+
+/*
 
     hero S(70,'C');
     S.print();
@@ -74,24 +157,7 @@ int main() {
     //R.health=suresh.health;
     //R.level = suresh.level;
     R.print();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+*/
 
 
 /*
@@ -131,8 +197,6 @@ int main() {
 */
      
 
-
-
     // //creation of object
     // hero ramesh; 
 
@@ -152,8 +216,5 @@ int main() {
 
     // cout<<sizeof(ramesh);
 
-
-
-    
     return 0;
 }
