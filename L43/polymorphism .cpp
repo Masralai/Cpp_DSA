9138353c21f78e0db577686b6357862caa430673
Masralai <devdeeppaul2004@gmail.com>
2025-01-03 18:39:51 +0530

    polymorphism

---
+++ L43/polymorphism .cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class A{    
+    public:
+    void sayHello(){
+        cout<<"hello "<<endl; 
+    }
+    void sayHello(){
+        cout<<"hello "<<endl; 
+    }
+
+};
+
+int main() {
+
+    A obj;
+    obj.sayHello();
+    
+    return 0;
+}
