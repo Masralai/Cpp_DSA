bb63d50d55503c78b1fbf5fc4a17d59a000021e0
Masralai <devdeeppaul2004@gmail.com>
2025-01-06 13:43:32 +0530

    linked_list

--- L30/default_args.cpp
+++ L30/default_args.cpp
@@ -7,6 +7,10 @@ void print(int arr[], int n , int start = 0){
     }
 }
 
+int add(int x , int y, int z=0,int w=0){
+    return x+y+z+w;
+}
+
 int main() {
 
     int arr[5]={1,4,7,8,9};
@@ -14,6 +18,12 @@ int main() {
     print(arr,size);
     cout<<endl;
     print(arr,size,2);
+
+
+
+    cout<<add(10,20)<<endl;
+    cout<<add(10,20,30)<<endl;
+    cout<<add(10,20,30,40)<<endl;
     
     return 0;
 }
