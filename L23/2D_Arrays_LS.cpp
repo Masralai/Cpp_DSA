2074f2d93896275adabce7fe78c8784ce48c1017
Masralai <devdeeppaul2004@gmail.com>
2025-01-20 06:10:36 +0530

    leet240_2DMatrix_BS2

--- L23/2D_Arrays_LS.cpp
+++ L23/2D_Arrays_LS.cpp
@@ -17,19 +17,19 @@ bool isPresent(int arr[][4],int target , int row , int col){//O(row*col)
 int main() {
 
     //2d arr initialize
-    int arr[3][4];
+    //int arr[3][4];
 
     //int arr[3][4]={1,2,3,4,5,6,7,8,9,10,14,16};//elements inputed row wise
-    //int arr[3][4]={{1,11,111,1111},{2,22,222,2222},{3,33,333,3333}};
-
+    int arr[3][4]={{1,11,111,1111},{2,22,222,2222},{3,33,333,3333}};
+    
 
     //taking input ->row wise input
 
-    for( int row = 0 ;row<3;row++){
-        for(int col=0;col<4;col++){
-            cin>>arr[row][col];
-        }
-    }
+    // for( int row = 0 ;row<3;row++){
+    //     for(int col=0;col<4;col++){
+    //         cin>>arr[row][col];
+    //     }
+    // }
 
     //taking input ->column wise input
 
@@ -50,7 +50,7 @@ int main() {
     cout<<" enter the element to search "<<endl;
 
     int target;
-    cin>>target;
+    //cin>>target;
 
     if(isPresent(arr,target,3,4)){
         cout<<" element found ";
@@ -59,6 +59,7 @@ int main() {
         cout<<"not found";
     }
 
+    
 
 
 
--- L23/Leet48_rotation_90.cpp
+++ L23/Leet48_rotation_90.cpp
@@ -4,6 +4,23 @@ using namespace std;
 
 void rotate(vector<vector<int>>& matrix) {
     
+    int len = matrix.size();
+    
+
+    //taking transpose 
+    for(int i=0;i<len;i++){
+        for(int j=i+1;j<len;j++){
+            
+            swap(matrix[i][j],matrix[j][i]);
+            
+        }
+    }
+
+    //reversing 
+
+    for(int i =0;i<len;i++){
+        reverse(matrix[i].begin(),matrix[i].end());
+    }
         
 }
 
@@ -14,15 +31,17 @@ int main() {
     matrix.push_back({1,2,3});
     matrix.push_back({4,5,6});
     matrix.push_back({7,8,9});
+    matrix.push_back({7,8,9});
 
     rotate(matrix);
 
     for( int row = 0 ;row<3;row++){
-        for(int col=0;col<4;col++){
+        for(int col=0;col<3;col++){
             cout<<matrix[row][col]<<" ";
         }cout<<endl;
     }
     
-    
+    // cout<<matrix.size(); //length of rows
+    // cout<<matrix[0].size(); //lengthn of cols
     return 0;
 }
