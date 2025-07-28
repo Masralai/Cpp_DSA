22ed9e35127494d393e9d14b84c49c45ae5590e9
Masralai <devdeeppaul2004@gmail.com>
2025-07-28 22:08:39 +0530

    A2Z sheet init

---
+++ L52/Clone_LL.cpp
@@ -0,0 +1,21 @@
+//https://www.geeksforgeeks.org/problems/clone-a-linked-list-with-next-and-random-pointer/1
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    int data;
+    Node *next;
+    Node *random;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+        random = NULL;
+    }
+};
+
+int main() {
+    
+    return 0;
+}
