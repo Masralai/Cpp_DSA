5ff420564d49cc5e123ccc9c6068d4bf3250d72a
Masralai <devdeeppaul2004@gmail.com>
2025-08-06 22:11:19 +0530

    deque to be fixed

--- L19/queue.cpp
+++ L19/queue.cpp
@@ -22,7 +22,28 @@ int main() {
     q.pop();
     cout<<"first element "<<q.front()<<endl;
     
-    cout<<"size after pop "<<q.size()<<endl;
+    cout<<"size after pop "<<q.size()<<"\n\n\n\n";
+
+    queue<int> num;
+    num.push(0);
+    num.push(1);
+    num.push(2);
+    num.push(3);
+    
+    int front = num.front();
+    int back = num.back();
+    int size =num.size();
+
+    cout<<"front "<<front << ", back "<<back<< ", size " <<size<<endl;
+    
+    cout<<"size is "<<num.size()<<endl;
+
+    for(;!num.empty(); num.pop()){
+        cout<<num.front()<<" ";
+    }
+    cout<<endl<<"size is "<<num.size()<<endl;
+
+
  
 
     return 0;
---
+++ L58/N_stacks.cpp
@@ -0,0 +1,11 @@
+//https://www.naukri.com/code360/problems/n-stacks-in-an-array_1164271
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+    
+    return 0;
+}
